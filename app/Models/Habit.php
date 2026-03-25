96fd19c7c8e351831c3afb9db108d030f723ea45
Helder-dlt162 <helder.drive162@gmail.com>
2026-03-24 23:06:43 -0500

    Database for Habit added

--- app/Models/Habit.php
+++ app/Models/Habit.php
@@ -4,7 +4,18 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class habit extends Model
+class Habit extends Model
 {
-    //
+
+    protected $table = 'habit';
+    
+    protected $fillable = [
+        'user_id',
+        'name',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
